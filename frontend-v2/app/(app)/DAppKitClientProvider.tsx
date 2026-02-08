87957f917c3a874d12a8cc618e7dd2a4431a0177
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-02-08 14:35:29 +0000

    Create DAppKitClientProvider.tsx

---
+++ frontend-v2/app/(app)/DAppKitClientProvider.tsx
@@ -0,0 +1,15 @@
+// app/DAppKitClientProvider.tsx
+"use client";
+
+import { DAppKitProvider, ConnectButton } from "@mysten/dapp-kit-react";
+import { dAppKit } from "./dapp-kit";
+
+export function DAppKitClientProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <DAppKitProvider dAppKit={dAppKit}>{children}</DAppKitProvider>;
+}
+
+export { ConnectButton };
