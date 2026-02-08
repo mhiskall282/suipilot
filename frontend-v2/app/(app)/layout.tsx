877a703fe46cf209d700bb0f9bf746119f6bdae1
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-02-08 14:34:29 +0000

    Update layout.tsx

--- frontend-v2/app/(app)/layout.tsx
+++ frontend-v2/app/(app)/layout.tsx
@@ -1,14 +1,21 @@
+"use client";
+// app/(app)/layout.tsx
 import { AppNavbar } from "@/components/app/app-navbar";
+import dynamic from "next/dynamic";
 
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const DAppKitClientProvider = dynamic(
+  () =>
+    import("./DAppKitClientProvider").then((mod) => mod.DAppKitClientProvider),
+  { ssr: false },
+);
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#050505] font-sans selection:bg-brand-accent/30 selection:text-white">
-      <AppNavbar />
-      {children}
-    </div>
+    <DAppKitClientProvider>
+      <div className="selection:bg-brand-accent/30 min-h-screen bg-[#050505] font-sans selection:text-white">
+        <AppNavbar />
+        {children}
+      </div>
+    </DAppKitClientProvider>
   );
 }
